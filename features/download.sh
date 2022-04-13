5a11f5889f86181e678525ccda75490cceeb150e
ehsansaleh <ehsan.slh@gmail.com>
2022-04-13 17:11:11 -0500

    pre-computed features download shell script

--- features/download.sh
+++ features/download.sh
@@ -9,13 +9,14 @@ SCRIPTDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPTDIR
 
 # importing the gdluntar helper function from the utils directory
-source ../utils_firth/bashfuncs.sh
+source ../utils/bashfuncs.sh
 
 FILEID="1Z-oO1hvcZkwsCZo9n7R3UGwHvFLi6Arf"
 FILENAME="s2m2r_features.tar"
 GDRIVEURL="https://drive.google.com/file/d/1Z-oO1hvcZkwsCZo9n7R3UGwHvFLi6Arf/view?usp=sharing"
 PTHMD5FILE="s2m2r_features.md5"
 REMOVETARAFTERDL="1"
-gdluntar ${FILEID} ${FILENAME} ${GDRIVEURL} ${PTHMD5FILE} ${REMOVETARAFTERDL}
+ISSMALL="0"
+gdluntar ${FILEID} ${FILENAME} ${GDRIVEURL} ${PTHMD5FILE} ${REMOVETARAFTERDL} ${ISSMALL}
 
 
